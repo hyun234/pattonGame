947adf2687ad104e08b6809ebd8e8e48046fdb39
재현 이 <marklio624@naver.com>
2022-06-01 19:40:12 +0900

    [ljh] lt

--- pattonGame/MGameLoop.hpp
+++ pattonGame/MGameLoop.hpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #pragma once
 #include <Windows.h>
 
@@ -8,6 +9,41 @@
 #pragma comment(lib,"OpenGL32")
 using namespace std;
 
+static GLuint texName;
+
+//비트맵 헤더를 한 묶음으로 다시 구조체로 묶었어요. 함수처리를 편하게 하려구요.
+typedef struct tagBITMAPHEADER {
+    BITMAPFILEHEADER bf;
+    BITMAPINFOHEADER bi;
+    RGBQUAD hRGB[256];
+}BITMAPHEADER;
+
+BYTE* LoadBitmapFile(BITMAPHEADER* bitmapHeader, int* imgSize, const char* filename)
+{
+    FILE* fp = fopen(filename, "rb");	//파일을 이진읽기모드로 열기
+    if (fp == NULL)
+    {
+        printf("파일로딩에 실패했습니다.\n");	//fopen에 실패하면 NULL값을 리턴
+        return NULL;
+    }
+    else
+    {
+        fread(&bitmapHeader->bf, sizeof(BITMAPFILEHEADER), 1, fp);	//비트맵파일헤더 읽기
+        fread(&bitmapHeader->bi, sizeof(BITMAPINFOHEADER), 1, fp);	//비트맵인포헤더 읽기
+        fread(&bitmapHeader->hRGB, sizeof(RGBQUAD), 256, fp);	//색상팔렛트 읽기
+
+        int imgSizeTemp = bitmapHeader->bi.biWidth * bitmapHeader->bi.biHeight;	//이미지 사이즈 계산
+        *imgSize = imgSizeTemp;	// 이미지 사이즈를 상위 변수에 할당
+
+        BYTE* image = (BYTE*)malloc(sizeof(BYTE) * imgSizeTemp);	//이미지크기만큼 메모리할당
+        fread(image, sizeof(BYTE), imgSizeTemp, fp);//이미지 크기만큼 파일에서 읽어오기
+        fclose(fp);
+
+
+        return image;
+    }
+}
+
 namespace LJH_Engine
 {
     class GameLoop
@@ -17,7 +53,7 @@ namespace LJH_Engine
         Enemy enemy;
         bool GameRunning;
         glfwWindow glwindow;
-        
+
     private:
         //다시 시작
         void reStart() {
@@ -44,11 +80,11 @@ namespace LJH_Engine
                 player.zeroPressed();
             }
         }
-
     public:
         void Run()
         {
             GameRunning = true;
+            init();
             glwindow.checkWindow();  //윈도우 조건 체크
             do
             {
@@ -58,102 +94,145 @@ namespace LJH_Engine
 
         }
     private:
+        void init(void)
+        {
+            glClearColor(0.0, 0.0, 0.0, 0.0);
+
+            BITMAPHEADER originalHeader;	//비트맵의 헤더부분을 파일에서 읽어 저장할 구조체
+            int imgSize;			//이미지의 크기를 저장할 변수
+            BYTE* image = LoadBitmapFile(&originalHeader, &imgSize, "lena_gray.bmp"); //비트맵파일을 읽어 화소정보를 저장
+            if (image == NULL) return;        //파일 읽기에 실패하면 프로그램 종료
+
+            glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+            glGenTextures(1, &texName);
+            glBindTexture(GL_TEXTURE_2D, texName);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+                GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                GL_NEAREST);
+
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 512, 512, 0, GL_RED, GL_BYTE, image);
+        }
+
         void Update() {
-            glClearColor(0, 0, 0, 0);
-            glClear(GL_COLOR_BUFFER_BIT);
-            //road width
-            glBegin(GL_QUADS);
 
-            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-            glVertex3f(-1.0f, 0.22f, 0.0f);
-            glVertex3f(-1.0f, 0.38f, 0.0f);
-            glVertex3f(1.0f, 0.38f, 0.0f);
-            glVertex3f(1.0f, 0.22f, 0.0f);
-            glEnd();
-
-            glBegin(GL_QUADS);
-
-            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-            glVertex3f(-1.0f, -0.08f, 0.0f);
-            glVertex3f(-1.0f, 0.08f, 0.0f);
-            glVertex3f(1.0f, 0.08f, 0.0f);
-            glVertex3f(1.0f, -0.08f, 0.0f);
-            glEnd();
-
-            glBegin(GL_QUADS);
-
-            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-            glVertex3f(-1.0f, -0.22f, 0.0f);
-            glVertex3f(-1.0f, -0.38f, 0.0f);
-            glVertex3f(1.0f, -0.38f, 0.0f);
-            glVertex3f(1.0f, -0.22f, 0.0f);
-            glEnd();
-
-            glBegin(GL_QUADS);
-            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-            glVertex3f(-0.08f, 0.3f, 0.0f);
-            glVertex3f(0.08f, 0.3f, 0.0f);
-            glVertex3f(0.08f, -0.3f, 0.0f);
-            glVertex3f(-0.08f, -0.3f, 0.0f);
-            glEnd();
-
-            ///player
-            glPointSize(30);
-            glBegin(GL_POINTS);
-            glColor3f(0.359375f, 0.99609375f, 0.81640625f);
-            glVertex2f(player.playerX, player.playerY);
-
-
-            //enemy
-            glPointSize(10);
-            glBegin(GL_POINTS);
-            glColor3f((float)(rand() % 100) / 100, (float)(rand() % 100) / 100, (float)(rand() % 100) / 100);
-            for (int i = 0; i < 3; i++) {
-                glVertex2f(enemy.enemyX[i], enemy.enemyY[i]);
-            }
-            glEnd();
-
-            //충돌
-            if (player.playerX == 0 && player.playerY == -0.3f)
-            {
-                if (enemy.enemyX[0] < 0.035f && enemy.enemyX[0] > -0.035f)
-                {
-                    GameRunning = false;
-                }
-            }
-            if (player.playerX == 0 && player.playerY == 0)
-            {
-                if (enemy.enemyX[1] < 0.035f && enemy.enemyX[1] > -0.035f)
-                {
-                    GameRunning = false;
-                }
-            }
-            if (player.playerX == 0 && player.playerY == 0.3f)
-            {
-                if (enemy.enemyX[2] < 0.035f && enemy.enemyX[2] > -0.035f)
-                {
-                    GameRunning = false;
-                }
-            }
 
             if (GameRunning == true)
             {
                 Input();
                 enemy.enemyCome();
+                glClearColor(0, 0, 0, 0);
+                glClear(GL_COLOR_BUFFER_BIT);
+                //road width
+                glBegin(GL_QUADS);
+
+                glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+                glVertex3f(-1.0f, 0.22f, 0.0f);
+                glVertex3f(-1.0f, 0.38f, 0.0f);
+                glVertex3f(1.0f, 0.38f, 0.0f);
+                glVertex3f(1.0f, 0.22f, 0.0f);
+                glEnd();
+
+                glBegin(GL_QUADS);
+
+                glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+                glVertex3f(-1.0f, -0.08f, 0.0f);
+                glVertex3f(-1.0f, 0.08f, 0.0f);
+                glVertex3f(1.0f, 0.08f, 0.0f);
+                glVertex3f(1.0f, -0.08f, 0.0f);
+                glEnd();
+
+                glBegin(GL_QUADS);
+
+                glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+                glVertex3f(-1.0f, -0.22f, 0.0f);
+                glVertex3f(-1.0f, -0.38f, 0.0f);
+                glVertex3f(1.0f, -0.38f, 0.0f);
+                glVertex3f(1.0f, -0.22f, 0.0f);
+                glEnd();
+
+                glBegin(GL_QUADS);
+                glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+                glVertex3f(-0.08f, 0.3f, 0.0f);
+                glVertex3f(0.08f, 0.3f, 0.0f);
+                glVertex3f(0.08f, -0.3f, 0.0f);
+                glVertex3f(-0.08f, -0.3f, 0.0f);
+                glEnd();
+
+                ///player
+                glPointSize(30);
+                glBegin(GL_POINTS);
+                glColor3f(0.359375f, 0.99609375f, 0.81640625f);
+                glVertex2f(player.playerX, player.playerY);
+
+
+                //enemy
+                glPointSize(10);
+                glBegin(GL_POINTS);
+                for (int i = 0; i < 3; i++) {
+                    glColor3f(1.0, 1.0, 0.0);
+                    glVertex2f(enemy.enemyX[i], enemy.enemyY[i]);
+                }
+                glEnd();
+
+                //충돌
+                if (player.playerX == 0 && player.playerY == -0.3f)
+                {
+                    if (enemy.enemyX[0] < 0.035f && enemy.enemyX[0] > -0.035f)
+                    {
+                        GameRunning = false;
+                    }
+                }
+                if (player.playerX == 0 && player.playerY == 0)
+                {
+                    if (enemy.enemyX[1] < 0.035f && enemy.enemyX[1] > -0.035f)
+                    {
+                        GameRunning = false;
+                    }
+                }
+                if (player.playerX == 0 && player.playerY == 0.3f)
+                {
+                    if (enemy.enemyX[2] < 0.035f && enemy.enemyX[2] > -0.035f)
+                    {
+                        GameRunning = false;
+                    }
+                }
             }
             if (GameRunning == false)
             {
-                cout << " 게임 종료";
-                glClearColor(1, 0, 0, 0);
-                glClear(GL_COLOR_BUFFER_BIT);
 
+                cout << " 게임 종료";
+                glClear(GL_COLOR_BUFFER_BIT);
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                
+                glEnable(GL_TEXTURE_2D);
+                glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+                glBindTexture(GL_TEXTURE_2D, texName);
+
+                glBegin(GL_QUADS);
+                glTexCoord2f(0.0, 0.0);  glVertex3f(-1.0f, -1.0f, 0.0f);
+                glTexCoord2f(0.0, 1.0);  glVertex3f(-1.0f, 1.0f, 0.0f);
+                glTexCoord2f(1.0, 1.0);  glVertex3f(1.0f, 1.0f, 0.0f);
+                glTexCoord2f(1.0, 0.0);  glVertex3f(1.0f, -1.0f, 0.0f);
                 glEnd();
+                /*glBegin(GL_TRIANGLES);
+                glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, 0.0);
+                glTexCoord2f(1.0, 0.0); glVertex3f(1.0, -1.0, 0.0);
+                glTexCoord2f(0.5, 1.0); glVertex3f(0.0, 1.0, 0.0);
+
+                glEnd();*/
+                glFlush();
+                glDisable(GL_TEXTURE_2D);
                 reStart();
             }
+            
+
+            
             glwindow.checkWindowEvent(); //이벤트 확인      
         }
     };
 }
-
-//glclear render() 
-//그림 update()
